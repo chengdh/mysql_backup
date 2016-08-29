d839579bb0f97ccd89fd392668f69daf6cbb45cc
chengdh <cheng.dongh@gmail.com>
2016-08-29 11:53:20 +0800

    comment for test

--- do_backup.sh
+++ do_backup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #echo "Creating mysql new binary log at `date`"
-if [ `date +%A` == "Sunday" -a `date +%H` == "06" -o "$1" == "dump" ]; then
+#if [ `date +%A` == "Sunday" -a `date +%H` == "06" -o "$1" == "dump" ]; then
   echo "Weekly Backup started `date`"
   echo "Full mysql database dump started"
   echo 'All existing full backups and binary log files will be removed'
@@ -13,10 +13,10 @@ if [ `date +%A` == "Sunday" -a `date +%H` == "06" -o "$1" == "dump" ]; then
   mysqldump -uroot -proot --flush-logs --delete-master-logs --master-data=2 --add-drop-table --lock-all-tables --databases yanzhao-mis_production > /var/lib/mysql/$DBFN
   bzip2 /var/lib/mysql/$DBFN
   echo "mysql dump complete"
-else
-  #echo "starting new bin log"
-  mysqladmin -uusername -ppassword flush-logs
-fi
+# else
+#   #echo "starting new bin log"
+#   mysqladmin -uusername -ppassword flush-logs
+# fi
 # newestlog=`ls -d /var/lib/mysql/mybinlog.?????? | sed 's/^.*\.//' | sort -g | tail -n 1`
 # for file in `ls /var/lib/mysql/mybinlog.??????`
 # do
