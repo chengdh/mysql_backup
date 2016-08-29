838a681a9b96d527daa47e82979e3ee28c57ee97
chengdh <cheng.dongh@gmail.com>
2016-08-29 11:52:14 +0800

    comment for test

--- do_backup.sh
+++ do_backup.sh
@@ -17,13 +17,13 @@ else
   #echo "starting new bin log"
   mysqladmin -uusername -ppassword flush-logs
 fi
-newestlog=`ls -d /var/lib/mysql/mybinlog.?????? | sed 's/^.*\.//' | sort -g | tail -n 1`
-for file in `ls /var/lib/mysql/mybinlog.??????`
-do
-  if [ "/var/lib/mysql/mybinlog.$newestlog" != "$file" ]; then
-    bzip2 "$file"
-  fi
-done
+# newestlog=`ls -d /var/lib/mysql/mybinlog.?????? | sed 's/^.*\.//' | sort -g | tail -n 1`
+# for file in `ls /var/lib/mysql/mybinlog.??????`
+# do
+#   if [ "/var/lib/mysql/mybinlog.$newestlog" != "$file" ]; then
+#     bzip2 "$file"
+#   fi
+# done
 
 #sftp -u 'ftpuser,ftppassword' backupspace.rimuhosting.com -e "set ftp:ssl-protect-data true; mirror -er --reverse -I *.bz2 -X $newestlog /var/lib/mysql /myvar/lib/mysql; mput /var/lib/mysql/mybinlog.index -O /myvar/lib/mysql; exit;"
 #echo "Bin Logs backed up"
