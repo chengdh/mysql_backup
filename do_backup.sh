503bb2c8f05a129aee2b818f63aa1bf01ecd1328
chengdh <cheng.dongh@gmail.com>
2016-08-29 13:05:05 +0800

    commit for update code

--- do_backup.sh
+++ do_backup.sh
@@ -1,29 +1,36 @@
 #!/bin/sh
+#you should run as root
 #echo "Creating mysql new binary log at `date`"
-#if [ `date +%A` == "Sunday" -a `date +%H` == "06" -o "$1" == "dump" ]; then
+if [ `date +%A` == "Sunday" -a `date +%H` == "06" -o "$1" == "dump" ]; then
   echo "Weekly Backup started `date`"
   echo "Full mysql database dump started"
   echo 'All existing full backups and binary log files will be removed'
   PREFIX='mysql-dump.'
   DT=`date "+%m%d%y"`
   DBFN=$PREFIX$DT'.sql'
+  DATABASES="il_yanzhao_new_production il_yanzhao_lite_production yanzhao-mis_production"
+  FULL_DUMPS_DIR="/var/mysql_dumps"
+  BIN_DUMPS_DIR="/var/log/mysql"
+  USER=root
+  PWD=root
+  mkdir -p $FULL_DUMPS_DIR
 
-  rm -f /var/lib/mysql/*.bz2
+  rm -f $FULL_DUMPS_DIR/*.bz2
 
-  mysqldump -uroot -proot --flush-logs --delete-master-logs --master-data=2 --add-drop-table --lock-all-tables --databases yanzhao-mis_production > /var/lib/mysql/$DBFN
-  bzip2 /var/lib/mysql/$DBFN
+  mysqldump -u$USER -p$PWD --flush-logs --delete-master-logs --master-data=2 --add-drop-table --lock-all-tables --databases $DATABASES > $FULL_DUMPS_DIR/$DBFN
+  bzip2 $FULL_DUMPS_DIR/$DBFN
   echo "mysql dump complete"
-# else
-#   #echo "starting new bin log"
-#   mysqladmin -uusername -ppassword flush-logs
-# fi
-# newestlog=`ls -d /var/lib/mysql/mybinlog.?????? | sed 's/^.*\.//' | sort -g | tail -n 1`
-# for file in `ls /var/lib/mysql/mybinlog.??????`
-# do
-#   if [ "/var/lib/mysql/mybinlog.$newestlog" != "$file" ]; then
-#     bzip2 "$file"
-#   fi
-# done
+else
+  #echo "starting new bin log"
+  mysqladmin -u$USER -p$PWD flush-logs
+fi
+newestlog=`ls -d $BIN_DUMPS_DIR/mysql-bin.?????? | sed 's/^.*\.//' | sort -g | tail -n 1`
+for file in `ls /$BIN_DUMPS_DIR/mysql-bin.??????`
+do
+  if [ "$BIN_DUMPS_DIR/mysql-bin.$newestlog" != "$file" ]; then
+    bzip2 "$file"
+  fi
+done
 
 #sftp -u 'ftpuser,ftppassword' backupspace.rimuhosting.com -e "set ftp:ssl-protect-data true; mirror -er --reverse -I *.bz2 -X $newestlog /var/lib/mysql /myvar/lib/mysql; mput /var/lib/mysql/mybinlog.index -O /myvar/lib/mysql; exit;"
 #echo "Bin Logs backed up"
