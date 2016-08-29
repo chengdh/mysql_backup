dc90a9ed715e122bde0aa74105a191afdc0ac913
chengdh <cheng.dongh@gmail.com>
2016-08-29 13:54:16 +0800

    fixed some bug

--- do_backup.sh
+++ do_backup.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 #you should run as root
-#echo "Creating mysql new binary log at `date`"
-if [ `date +%A` == "Sunday" -a `date +%H` == "06" -o "$1" == "dump" ]; then
+#echo "Bin Logs backed up"
+FULL_DUMPS_DIR="/var/mysql_dumps"
+BIN_DUMPS_DIR="/var/log/mysql"
+USER="root"
+PASSWD="root"
+
+if [`date +%A` == "Sunday"] && [`date +%H` == "06"] || ["$1" == "dump"]; then
   echo "Weekly Backup started `date`"
   echo "Full mysql database dump started"
   echo 'All existing full backups and binary log files will be removed'
@@ -9,22 +14,18 @@ if [ `date +%A` == "Sunday" -a `date +%H` == "06" -o "$1" == "dump" ]; then
   DT=`date "+%m%d%y"`
   DBFN=$PREFIX$DT'.sql'
   DATABASES="il_yanzhao_new_production il_yanzhao_lite_production yanzhao-mis_production"
-  FULL_DUMPS_DIR="/var/mysql_dumps"
-  BIN_DUMPS_DIR="/var/log/mysql"
-  USER=root
-  PWD=root
   mkdir -p $FULL_DUMPS_DIR
-
   rm -f $FULL_DUMPS_DIR/*.bz2
 
-  mysqldump -u$USER -p$PWD --flush-logs --delete-master-logs --master-data=2 --add-drop-table --lock-all-tables --databases $DATABASES > $FULL_DUMPS_DIR/$DBFN
+  mysqldump -u$USER -p$PASSWD --flush-logs --delete-master-logs --master-data=2 --add-drop-table --lock-all-tables --databases $DATABASES > $FULL_DUMPS_DIR/$DBFN
   bzip2 $FULL_DUMPS_DIR/$DBFN
   echo "mysql dump complete"
 else
-  #echo "starting new bin log"
-  mysqladmin -u$USER -p$PWD flush-logs
+  echo "starting new bin log $USER"
+  mysqladmin -u$USER -p$PASSWD flush-logs
 fi
 newestlog=`ls -d $BIN_DUMPS_DIR/mysql-bin.?????? | sed 's/^.*\.//' | sort -g | tail -n 1`
+echo $newstlog
 for file in `ls /$BIN_DUMPS_DIR/mysql-bin.??????`
 do
   if [ "$BIN_DUMPS_DIR/mysql-bin.$newestlog" != "$file" ]; then
